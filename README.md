57eb85a11e1e5e7f13bb49d219d8218d2c437211
Amey Parulekar <amey@tetra.ai>
2022-10-24 21:39:17 -0700

    Add LOCK and UNLOCK handlers.

---
+++ README.md
@@ -0,0 +1,5 @@
+Start with VAULT_TOKEN set, and
+
+```
+NODE_EXTRA_CA_CERTS=/path/to/ca.pem npm run start
+```
--- server.js
+++ server.js
@@ -2,48 +2,65 @@ const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+const state_mount = "terraform-state"
+
 function dumpAll(req) {
-    console.log(`Im called with the method [${req.method}], params [${JSON.stringify(req.params)}], query [${JSON.stringify(req.query)}], body [${JSON.stringify(req.body)}]`);
+    console.error(`Called with the method [${req.method}], params [${JSON.stringify(req.params)}], query [${JSON.stringify(req.query)}], body [${JSON.stringify(req.body)}]`);
 }
 
 let token = process.env.TOKEN;
-let caFile = process.env.CA_FILE
 
 let options = {
     apiVersion: 'v1', // default
     endpoint: 'https://vault.wirywolf.com', // default
-    token: token,
-    requestOptions: {
-        caFile: caFile
-    }
+    token: token
 };
 
 // get new instance of the client
 let vault = require("node-vault")(options);
 
-vault.tokenRenewSelf().then((output) => {
-    console.log("Token renewed.");
-})
-
 app.use(express.json({limit: "100mb", parameterLimit: 100000000}))
 app.listen(port);
-console.log('book list RESTful API server started on: ' + port);
+console.log('API server started on: ' + port);
 
 app.get('/:module', (req, res) => {
-    vault.read(`terraform/data/${req.params.module}/state`).then((output) => {
-        console.log(output);
+    vault.read(`${state_mount}/data/${req.params.module}/state`).then((output) => {
+        console.debug(`State ${req.params.module} returned to client`);
         res.status(200).json(output.data.data).end();
-    }).catch((output) => {
-        console.log(output);
+    }).catch((err) => {
+        console.error(`State ${req.params.module} not found`)
         res.status(404).end();
     })
 }).post('/:module', (req, res) => {
-    vault.write(`terraform/data/${req.params.module}/state`, { data: req.body }).then((output) => {
+    vault.write(`${state_mount}/data/${req.params.module}/state`, { data: req.body }).then((output) => {
         res.status(200).end();
+    })
+}).lock('/:module', (req, res) => {
+    console.debug(`State ${req.params.module} locked by ${req.body.ID}`)
+    vault.write(`${state_mount}/data/${req.params.module}/lock`, { data: req.body }).then((output) => {
+        res.status(200).end();
+    })
+}).unlock('/:module', (req, res) => {
+    vault.read(`${state_mount}/data/${req.params.module}/lock`).then((output) => {
+        if (output.data.data.ID == req.body.ID) {
+            vault.delete(`${state_mount}/data/${req.params.module}/lock`).then((output) => {
+                console.debug(`State ${req.params.module} unlocked by ${req.body.ID}`)
+                res.status(200).end()
+            }).catch((err) => {
+                console.error(`Could not unlock ${req.body.ID} because: ${output}`)
+                res.status(500).end()
+            })
+        } else {
+            console.error(`Lock is not held by ${req.body.ID}}`)
+            res.status(409).end()
+        }
+    }).catch((err) => {
+        console.error(`Lock not held by anyone`)
+        res.status(404).end()
     })
 }).all('/:module', (req, res) => {
     dumpAll(req);
-    res.status(400).end();
+    res.status(405).end();
 });
 
 
